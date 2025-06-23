d83a099ee56910fd3bd96a0334486fcbe3226f4a
Ifchu <Ifchu0233@protonmail.com>
2025-06-24 00:21:22 +0300

    Update header and add style

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,8 +4,8 @@ export default function Header() {
   return (
     <header>
       <img src="/src/assets/" alt=" icon" />
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/login">Login</Link> | <Link to="/register">Register</Link>
+      <nav className="links">
+        <Link to="/">Home</Link>  <Link to="/login">Login</Link> <Link to="/register">Register</Link>
       </nav>
       <h1>Random Color</h1>
     </header>
