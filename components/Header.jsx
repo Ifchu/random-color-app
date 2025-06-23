023567025fa9f1d42c8f447277e3811360f9d69d
Ifchu <Ifchu0233@protonmail.com>
2025-06-24 00:49:34 +0300

    Adding logo and edit some css and header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,47 @@
-import { Link } from "react-router-dom";
+import { Link , useNavigate } from "react-router-dom";
+import { useState, useEffect } from 'react';
 
-export default function Header() {
+
+
+
+function Header() {
+
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+      
+  useEffect(() => {
+    const saved = localStorage.getItem("user");
+    if (saved) {
+      setUser(JSON.parse(saved));
+    }
+  }, []);
+
+    const handleLogout = () => {
+    localStorage.removeItem("user");
+    setUser(null);
+    navigate("/login");
+  };
+
   return (
     <header>
-      <img src="/src/assets/" alt=" icon" />
-      <nav className="links">
-        <Link to="/">Home</Link>  <Link to="/login">Login</Link> <Link to="/register">Register</Link>
-      </nav>
+      <img src="/src/assets/palette-color.png" alt=" palette color icon" />
+      <nav style={{ padding: "1rem", background: "transparent" }}>
+      <Link to="/">Home</Link> |{" "}
+      {!user ? (
+        <>
+          <Link to="/login">Login</Link> |{" "}
+          <Link to="/register">Register</Link>
+        </>
+      ) : (
+        <>
+          <span>Здравей, {user.name}!</span> |{" "}
+          <button onClick={handleLogout}>Изход</button>
+        </>
+      )}
+    </nav>
       <h1>Random Color</h1>
     </header>
   );
 }
+
+export default Header
